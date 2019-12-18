f4b8d288af9870ac25e42f2d728cc781cb5ac9b6
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-12-18 23:14:29 +0000

    created a dictionary to lookup the move from for player1.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,19 @@
 class TicTacToe
-  def move(player_move)
-    'X||'
+  def initialize
+    @game = []
+  end
+
+  def play(move)
+    @move = move
+    round(@move)
+  end
+
+  def round(move)
+    @the_board = {
+      top_L: 'X||', top_M: '', top_R: '',
+      mid_L: '', mid_M: '', mid_R: '',
+      low_L: '', low_M: '', low_R: ''
+    }
+    @game << @the_board[move]
   end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,10 +2,11 @@ require 'tic_tac_toe'
 
 describe TicTacToe do
   let(:tic_tac_toe) { described_class.new }
+  let(:move) { :top_L }
 
   context 'Player turns between positions' do
-    it 'takes top-L from Player-X and returns X | |' do
-      expect(tic_tac_toe.move('top-L')).to eq 'X||'
+    it 'takes :top_L from Player-X and returns X | |' do
+      expect(tic_tac_toe.play(move)).to eq ['X||']
     end
   end
 end
