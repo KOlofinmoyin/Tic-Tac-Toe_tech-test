9304826dc6eaa544c241c67652e7b1d30555c032
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-12-19 15:56:02 +0000

    takes :mid_M from Player X, then returns |X|. Not sure this is passing for rhe right reason. Will investigate/refactor further.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,29 +7,34 @@ class Play
   def play(move, turn)
     @move = move
     @turn = turn
-    round(@move, @turn)
+    record_the_round(@move, @turn)
   end
 
-  def round(move, turn)
+  def record_the_round(move, turn)
     @the_board = {
       top_L: '', top_M: '', top_R: '',
       mid_L: '', mid_M: '', mid_R: '',
       low_L: '', low_M: '', low_R: ''
     }
 
-    def print_board
-      @print_board.print_board(@game)
-    end
-
     case turn
     when 'X||'
       @the_board[move] = turn
       @game << @the_board[move]
+
+    when '|X|'
+      @the_board[move] = turn
+      @game << @the_board[move]
+
     when 'O||'
       @the_board[move] = turn
       @game << @the_board[move]
     else
       puts 'Unknown input. Try again!'
     end
+  end
+
+  def print_board
+    @print_board.print_board(@game)
   end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,22 +2,36 @@ require 'tic_tac_toe'
 
 describe Play do
   let(:tic_tac_toe) { described_class.new }
-  let(:move) { :top_L }
 
   context 'Player turns between positions' do
-    describe 'Player X turn' do
-      let(:turn) { 'X||' }
 
-      it 'takes :top_L from Player-X and returns X||' do
-        expect(tic_tac_toe.play(move, turn)).to eq ['X||']
+    describe 'Player X turn' do
+      context "Playing to the top-Left position" do
+        let(:move) { :top_L }
+        let(:turn) { 'X||' }
+
+        it 'takes :top_L from Player-X and returns X||' do
+          expect(tic_tac_toe.play(move, turn)).to eq ['X||']
+        end
+      end
+
+      context "Playing to the middle-Middle position" do
+        let(:move) { :mid_M }
+        let(:turn) { '|X|' }
+        it "takes :mid_M from Player-X and returns |X|" do
+          expect(tic_tac_toe.play(move, turn)).to eq ['|X|']
+        end
       end
     end
 
     describe 'Player O turn' do
-      let(:turn) { 'O||' }
+      context "Playing to the top-Left position" do
+        let(:move) { :top_L }
+        let(:turn) { 'O||' }
 
-      it 'takes :top_L from Player-O and returns O||' do
-        expect(tic_tac_toe.play(move, turn)).to eq ['O||']
+        it 'takes :top_L from Player-O and returns O||' do
+          expect(tic_tac_toe.play(move, turn)).to eq ['O||']
+        end
       end
     end
   end
